2cfc42a0d350ac8c1ab34a3bec7aefe36601a485
L. S. Junior <90662061+LsTheJunior@users.noreply.github.com>
2022-01-25 00:02:07 -0300

    Update views.py

--- exchangerates/views.py
+++ exchangerates/views.py
@@ -1,51 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from controller import CallApi
+
+
 
 def home(request):
 
-    apiresponse = GetData()
+    apiresponse = CallApi.GetData()
     data = apiresponse.rates()
-    USD_series = list()
+    EUR_series = list()
     JPY_series = list()
     BRL_series = list()
-    USD_series = data['USD'].tolist()
+    Dates_series = list()
+    EUR_series = data['EUR'].tolist()
     JPY_series = data['JPY'].tolist()
     BRL_series = data['BRL'].tolist()
-
+    Dates_series = data['Data'].tolist()
+    Dates_series.reverse()
 
     chart = {
 
-        'series': [USD_series, JPY_series, BRL_series]
+        'series': [EUR_series, JPY_series, BRL_series]
     }
-    contexto = {"USD_series": USD_series, 'JPY_series': JPY_series, 'BRL_series':BRL_series}
+    contexto = {"EUR_series": EUR_series, 'JPY_series': JPY_series, 'BRL_series':BRL_series, 'Dates_series':Dates_series}
 
     return render(request, 'rates.html', context=contexto)
 
-class GetData:
-    def rates(self):
-       import requests
-       import  pandas as pd
-       dataset = pd.DataFrame()
-       df = pd.DataFrame(columns=['USD','JPY','BRL'])
 
-       i= 0
-       while i < 5:
-
-            import datetime as dt
-            today = dt.date.today()
-            data = today - dt.timedelta(days=i)
-            data = data.strftime('%Y-%m-%d')
-            url = f'https://api.vatcomply.com/rates?date={data}'
-            payload = ""
-            headers = {}
-            response = requests.request("GET", url, headers=headers, data=payload)
-            res_json = response.json()
-            resultado = res_json['rates']
-            dataset = dataset.append({'USD':resultado['USD'] ,'JPY':resultado['JPY'],'BRL':resultado['BRL']},ignore_index=True)
-            i+=1
-
-       return dataset
 
 
 
